fbb644e6e2625f9b28d89552c1e6ef872a5634c8
Fabian Marin <fabian-guitar@hotmail.com>
2019-09-19 13:23:59 -0500

    build

--- src/index.js
+++ src/index.js
@@ -22,6 +22,12 @@ app.use(cors());
 //Routes
 app.use('/api', indexRoutes);
 
+// The "catchall" handler: for any request that doesn't
+// match one above, send back React's index.html file.
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname+'/client/build/index.html'));
+});
+
 //Mongoose
 mongoose.Promise = global.Promise;
 mongoose.set('debug', true);
